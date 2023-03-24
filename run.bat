a67ee9da1a0094bb7e1181ff529c223f1603f981
MartinKratochvilProgramy <martvil96@gmail.com>
2023-03-24 14:21:48 +0100

    created test up to start game

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 start node "C:\Users\martv\Desktop\scratch\Bang_online\server\build\server.js"
 
+start client.bat
+
 python -B "c:\Users\martv\Desktop\scratch\selenium_testing\main.py"
 
 taskkill /FI "WINDOWTITLE eq C:\Program Files\nodejs\node.exe*" /F
--- server.bat
+++
@@ -1,1 +0,0 @@
-node "c:\Users\martv\Desktop\scratch\Bang_online\server\build\server.js"
