672c6931b7e8746548c06d218e05d40454785a8a
MartinKratochvilProgramy <martvil96@gmail.com>
2023-03-25 06:47:22 +0100

    test bang, vulcanic

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set TEST_PLAYER=fgbwrighoqfqwehfnwog
 start node "C:\Users\martv\Desktop\scratch\Bang_online\server\build\server.js"
 
-python -B "c:\Users\martv\Desktop\scratch\selenium_testing\main.py"
+python -B "c:\Users\martv\Desktop\scratch\Bang_online_testing\main.py"
 
 taskkill /FI "WINDOWTITLE eq C:\Program Files\nodejs\node.exe*" /F
